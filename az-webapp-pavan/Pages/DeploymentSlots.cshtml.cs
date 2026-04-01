a89f8d266298b93ad049d6d8a4568b2e74a2f4c6
pavangopal <pavangopal913@gmail.com>
2026-04-01 16:00:23 +0530

    added staging branch

---
+++ az-webapp-pavan/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_webapp_pavan.Pages
+{
+    public class DeployementSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
